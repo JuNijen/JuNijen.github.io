8bf97cce5697ea6820d8207e7567e11bec9051e6
JuNijen <wjstldms0823@naver.com>
2022-08-12 02:08:13 +0900

    [EDIT] very little code style
    Signed-off-by: JuNijen <wjstldms0823@naver.com>

--- vanillajs-challenge/js/Day8.js
+++ vanillajs-challenge/js/Day8.js
@@ -1,12 +1,11 @@
-
 // 실시간으로 범위 값을 업데이트해야 합니다.
 // 유저가 숫자를 선택한 후에 게임을 플레이할 수 있습니다.
 // 유저에게 게임의 승패를 알려야 합니다.
 
-const arrangeForm    = document.querySelector("#arrange-input input");
-const guessForm      = document.querySelector("#user-input input");
-const submitButton   = document.querySelector("#user-input button");
-const answerText     = document.querySelector("#answer");
+const arrangeForm = document.querySelector("#arrange-input input");
+const guessForm = document.querySelector("#user-input input");
+const submitButton = document.querySelector("#user-input button");
+const answerText = document.querySelector("#answer");
 const gameResultText = document.querySelector("#game-result");
 const HIDDEN_CLASSNAME = "hidden"
 
@@ -18,29 +17,27 @@ console.dir(submitButton);
 // 참고자료 : https://hianna.tistory.com/454
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
 
-function onSubmit(event){
+function onSubmit(event) {
     event.preventDefault();
 
-    const randomArrange = parseInt(arrangeForm.value);    //랜덤 범위 입력을 받는 폼
-    const userInput     = parseInt(guessForm.value);      //추측 정답 입력을 받는 폼
-    
+    const randomArrange = parseInt(arrangeForm.value); //랜덤 범위 입력을 받는 폼
+    const userInput = parseInt(guessForm.value); //추측 정답 입력을 받는 폼
+
     // 0에서 사용자가 지정한 숫자까지의 범위에서 랜덤 한 숫자를 찾으세요. (범위는 0 이상 입력값 이하가 됩니다.)
     // 범위에는 음수가 포함될 수 없습니다.
-    if(randomArrange > 0 && userInput > 0){
+    if (randomArrange > 0 && userInput > 0) {
 
         //랜덤한 숫자를 생성 해 주고 화면에 가시적으로 표기 해 준다.
         const randomNumber = rand(1, randomArrange);
         answerText.innerText = `you chose: ${userInput}, the machine chose: ${randomNumber}`;
 
         //숫자가 서로 같은지를 체크한다.
-        if(userInput === randomNumber)
-        {
+        if (userInput === randomNumber) {
             gameResultText.innerText = "You won!";
-        }
-        else{
+        } else {
             gameResultText.innerText = "You lost!";
         }
 
